2f3ff44f62351878cf35bdf61c0c2c491c9a860c
Unknown <keith.patrick.morgan@gmail.com>
2018-10-30 16:24:00 -0600

    v2 now working and rendering in panel

--- switcheroo.py
+++ switcheroo.py
@@ -6,41 +6,31 @@ bl_info = {
 
 import bpy
 
-
-class Switcheroo(bpy.types.Panel):
-    bl_label = "Switcheroo"
-    bl_id = "view3D.custom_menu"
-    bl_space_type = "PROPERTIES"
-    bl_region_type = "WINDOW"
-    bl_context = "render"
-
-
-    def draw(self, context):
-        layout = self.layout
-        split = layout.split
-
-        #Layout
-        row = layout.row()
-        row.operator('switcheroo.switch', icon='ARROW_LEFTRIGHT')
-
-
 class OBJECT_BUTTON(bpy.types.Operator):
     bl_idname = "switcheroo.switch"
     bl_label = "Switch X/Y"
-
 
     def execute(self, context):
         bpy.context.scene.render.resolution_x, bpy.context.scene.render.resolution_y = bpy.context.scene.render.resolution_y, bpy.context.scene.render.resolution_x
         return {'FINISHED'}
 
+def draw_switcheroo(self, context):
+    layout = self.layout
+    split = layout.split
+
+    #Layout
+    row = layout.row()
+    row.operator('switcheroo.switch', icon='ARROW_LEFTRIGHT')
 
 def register():
-    bpy.utils.register_class(Switcheroo)
+    # lets add the menu to the Mesh Display panel via append or prepend
     bpy.utils.register_class(OBJECT_BUTTON)
+    bpy.types.RENDER_PT_dimensions.append(draw_switcheroo)
 
 def unregister():
-    bpy.utils.unregister_class(Switcheroo)
+    # remove the menu
     bpy.utils.unregister_class(OBJECT_BUTTON)
+    bpy.types.RENDER_PT_dimensions.remove(draw_switcheroo)
 
 if __name__ == "__main__":
     register()
--- switcheroo_v2.py
+++ switcheroo_v2.py
@@ -1,77 +1,38 @@
 bl_info = {
     "name": "Switcheroo",
     "category": "Render",
-    "author": "Keith Morgan"
+    "author": "Keith Morgan",
+    "location": "Properties > Render > Dimensions",
+    "description": "Swaps the X/Y render dimensions.",
 }
 
 import bpy
-from bpy.types import Menu, Panel
 
-class RenderButtonsPanel:
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = 'WINDOW'
-    bl_context = "render"
+def draw_switcheroo(self, context):
+    layout = self.layout
+    split = layout.split
 
-    @classmethod
-    def poll(cls, context):
-        scene = context.scene
-        return scene and (scene.render.engine in cls.COMPAT_ENGINES)
-#class Switcheroo(bpy.types.Panel):
-class RENDER_PT_dimensions(RenderButtonsPanel, Panel):
-    bl_label = "Dimensions"
-   # bl_label = "Switcheroo"
-   # bl_id = "view3D.custom_menu"
-    bl_space_type = "PROPERTIES"
-    bl_region_type = "WINDOW"
-    bl_context = "render"
-
-
-    def draw(self, context):
-        layout = self.layout
-        split = layout.split
-
-        #Layout
-        row = layout.row()
-        row.operator('switcheroo.switch', icon='ARROW_LEFTRIGHT')
-
+    #Layout
+    row = layout.row()
+    row.operator('switcheroo.switch', icon='ARROW_LEFTRIGHT')
 
 class OBJECT_BUTTON(bpy.types.Operator):
     bl_idname = "switcheroo.switch"
     bl_label = "Switch X/Y"
-
+    bl_description = "Flip between portrait and landscape camera orientations"
 
     def execute(self, context):
         bpy.context.scene.render.resolution_x, bpy.context.scene.render.resolution_y = bpy.context.scene.render.resolution_y, bpy.context.scene.render.resolution_x
         return {'FINISHED'}
 
-def menu_func(self, context):
-    ui_layout(self.layout, context)
-
-classes = (
-    RenderButtonsPanel,
-    RENDER_PT_dimensions,
-    OBJECT_BUTTON,
-)
-
 
 def register():
-
-   bpy.types.RENDER_PT_performance.append(menu_func)
-   # bpy.utils.register_class(RenderButtonsPanel)
-   bpy.utils.register_class(OBJECT_BUTTON)
+    bpy.types.RENDER_PT_dimensions.append(draw_switcheroo)
+    bpy.utils.register_class(OBJECT_BUTTON)
 
 def unregister():
-#    bpy.utils.unregister_class(RenderButtonsPanel)
-  # bpy.utils.unregister_class(RENDER_PT_dimensions)
-   # bpy.utils.unregister_class(RenderButtonsPanel)
-   bpy.types.RENDER_PT_performance.remove(menu_func)
-   bpy.utils.unregister_class(OBJECT_BUTTON)
-
+    bpy.utils.unregister_class(OBJECT_BUTTON)
+    bpy.types.RENDER_PT_dimensions.remove(draw_switcheroo)
 
 if __name__ == "__main__":
-   register()
-
-# if __name__ == "__main__":  # only for live edit.
-#     from bpy.utils import register_class
-#     for cls in classes:
-#         register_class(cls)
+    register()
