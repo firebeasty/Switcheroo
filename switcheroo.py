b40d389003f42ec9e845d872c5414011c963b1aa
Unknown <keith.patrick.morgan@gmail.com>
2018-11-05 14:18:46 -0700

    Consolidated v1 and v2 files for 2.79 version. Deleted old version 2

--- switcheroo.py
+++ switcheroo.py
@@ -1,34 +1,38 @@
 bl_info = {
     "name": "Switcheroo",
     "category": "Render",
-    "author": "Keith Morgan"
+    "version": (1, 1, 0),
+    "blender": (2, 79, 0),
+    "author": "Keith Morgan",
+    "location": "Properties > Render > Dimensions",
+    "description": "Swaps the X/Y render dimensions.",
 }
 
 import bpy
 
+#Draw Button
+def draw_switcheroo(self, context):
+    layout = self.layout
+    split = layout.split
+
+    row = layout.row()
+    row.operator('switcheroo.switch', icon='ARROW_LEFTRIGHT')
+
+#Switcheroo Script
 class SWITCHEROO_EXECUTE_BUTTON(bpy.types.Operator):
     bl_idname = "switcheroo.switch"
     bl_label = "Switch X/Y"
+    bl_description = "Flip between portrait and landscape camera orientations"
 
     def execute(self, context):
         bpy.context.scene.render.resolution_x, bpy.context.scene.render.resolution_y = bpy.context.scene.render.resolution_y, bpy.context.scene.render.resolution_x
         return {'FINISHED'}
 
-def draw_switcheroo(self, context):
-    layout = self.layout
-    split = layout.split
-
-    #Layout
-    row = layout.row()
-    row.operator('switcheroo.switch', icon='ARROW_LEFTRIGHT')
-
 def register():
-    # lets add the menu to the Mesh Display panel via append or prepend
     bpy.utils.register_class(SWITCHEROO_EXECUTE_BUTTON)
     bpy.types.RENDER_PT_dimensions.append(draw_switcheroo)
 
 def unregister():
-    # remove the menu
     bpy.utils.unregister_class(SWITCHEROO_EXECUTE_BUTTON)
     bpy.types.RENDER_PT_dimensions.remove(draw_switcheroo)
 
